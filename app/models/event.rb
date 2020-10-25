7e165da8dd57246d993b954943966479d0f45a02
Giuliana Sturion <sturiongiuliana@gmail.com>
2020-10-25 13:34:54 -0300

    Navegação entre paginas

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  has_many :works
+  has_many :artists, through: :works
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :artists, only: [ :index, :show ]
+    resources :artists, only: [ :index ]
   end
-  root :to => "events#index"
+  root :to => "artists#index"
 end
